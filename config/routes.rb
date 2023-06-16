0571a5944bb71c46e8e29102df1fc0316be6a208
316hayato <131685231+316hayato@users.noreply.github.com>
2023-06-16 17:15:48 +0000

    home用ページ・コントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
+# 会員側のルーティング設定
+  root to: "homes#top"
+  get 'homes/about' => 'homes#about', as: 'about'
+  resources :items
   devise_for :customers
+
+# 管理者側のルーティング設定
+  namespace :admin do
+    resources :items
+  end
+  devise_for :admins
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
