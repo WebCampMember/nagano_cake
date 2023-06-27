4e2c6c1f4c082b68bc9731633319b6e0da7c4183
316hayato <131685231+316hayato@users.noreply.github.com>
2023-06-27 15:09:29 +0000

    管理者側以外の商品一覧・詳細ページ作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
-  
+
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
