f3ceee3f72fd392ad198855874fd6bedb41870d0
316hayato <131685231+316hayato@users.noreply.github.com>
2023-07-18 17:54:24 +0000

    レイアウト作成(管理者：商品、ジャンル)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,9 +4,9 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to admin_item_path(@item)
+    item = Item.new(item_params)
+    if item.save
+      redirect_to admin_item_path(item)
     else
       render :new
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,12 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :ordre_details, dependent: :destroy
 
+  validates :image, presence: true
+  validates :genre_id, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
