cc84fa07d5f11f668aa53893d1d580105ae739fb
316hayato <131685231+316hayato@users.noreply.github.com>
2023-07-22 19:57:05 +0000

    微修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -30,4 +31,25 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  protected
+    # 退会しているかを判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted
+    ## 【処理内容3】
+      sign_in(@customer)
+      redirect_to root_path, notice: 'ログインに成功しました。'
+    elsif @customer.is_deleted
+       # 退会している場合はサインアップ画面に遷移し、アラートメッセージを表示
+      redirect_to new_customer_registration_path, alert: '退会済みのアカウントです。'
+    else
+      # アカウントが存在しないか、パスワードが一致しない場合
+      redirect_to new_customer_session_path, alert: 'メールアドレスまたはパスワードが間違っています。'
+    end
+  end
 end
